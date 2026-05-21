8d67d4e6395a1f1f08d740d06152c2e147c9cf23
SeanLinzx <zxnb@hnu.edu.cn>
2026-05-21 23:39:32 +0800

    Update pages to revised Figma SVG exports

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const DESIGN = {
   width: 1440,
   height: 3728,
   images: {
-    zh: "./111%20CH.svg",
-    en: "./111%20EN.svg",
+    zh: "./111%20CH-2.svg",
+    en: "./111%20EN-2.svg",
   },
   alt: {
     zh: "人智协同设计 学科创新引领基地",
